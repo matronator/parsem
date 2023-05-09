f93430d62bcbe581d21a504d60243fdd22e0bc88
Matronator <5470780+matronator@users.noreply.github.com>
2023-05-09 16:18:17 +0200

    remove all slashes

--- src/Parsem/Filters.php
+++ src/Parsem/Filters.php
@@ -28,39 +28,39 @@ class Filters
 
     public static function lowerFirst(string $string): string
     {
-        $fc = \mb_strtolower(\mb_substr($string, 0, 1, static::ENCODING), static::ENCODING);
-        return $fc . \mb_substr($string, 1, null, static::ENCODING);
+        $fc = mb_strtolower(mb_substr($string, 0, 1, static::ENCODING), static::ENCODING);
+        return $fc . mb_substr($string, 1, null, static::ENCODING);
     }
 
     public static function first(string $string): string
     {
-        return \mb_substr($string, 0, 1, static::ENCODING);
+        return mb_substr($string, 0, 1, static::ENCODING);
     }
 
     public static function last(string $string): string
     {
-        return \mb_substr($string, -1, 1, static::ENCODING);
+        return mb_substr($string, -1, 1, static::ENCODING);
     }
 
     public static function camelCase(string $string): string
     {
-        $string = \str_replace(['-', '_'], ' ', $string);
-        $string = \ucwords($string);
-        $string = \str_replace(' ', '', $string);
+        $string = str_replace(['-', '_'], ' ', $string);
+        $string = ucwords($string);
+        $string = str_replace(' ', '', $string);
         return $string;
     }
 
     public static function snakeCase(string $string): string
     {
-        $string = \preg_replace('/([a-z])([A-Z])/', '$1_$2', $string);
-        $string = \strtolower($string);
+        $string = preg_replace('/([a-z])([A-Z])/', '$1_$2', $string);
+        $string = strtolower($string);
         return $string;
     }
 
     public static function kebabCase(string $string): string
     {
-        $string = \preg_replace('/([a-z])([A-Z])/', '$1-$2', $string);
-        $string = \strtolower($string);
+        $string = preg_replace('/([a-z])([A-Z])/', '$1-$2', $string);
+        $string = strtolower($string);
         return $string;
     }
 
@@ -73,31 +73,31 @@ class Filters
 
     public static function titleCase(string $string): string
     {
-        $string = \str_replace(['-', '_'], ' ', $string);
-        $string = \ucwords($string);
+        $string = str_replace(['-', '_'], ' ', $string);
+        $string = ucwords($string);
         return $string;
     }
 
     public static function length(string $string): int
     {
-        return \mb_strlen($string, static::ENCODING);
+        return mb_strlen($string, static::ENCODING);
     }
 
     public static function reverse(string $string): string
     {
-        return \strrev($string);
+        return strrev($string);
     }
 
     public static function random(array|string $array): mixed
     {
-        if (\is_string($array))
-            return $array[\rand(0, \strlen($array) - 1)];
+        if (is_string($array))
+            return $array[rand(0, strlen($array) - 1)];
         
-        return $array[\array_rand($array)];
+        return $array[array_rand($array)];
     }
 
     public static function truncate(string $string, int $length, string $ending = '...'): string
     {
-        return \mb_substr($string, 0, $length, static::ENCODING) . $ending;
+        return mb_substr($string, 0, $length, static::ENCODING) . $ending;
     }
 }
